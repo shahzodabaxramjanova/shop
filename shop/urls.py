934f60a0ffb7fca6837ab7ff16a1bdffe4016030
shahzodabaxramjanova <bdshahzoda@gmail.com>
2026-06-28 18:13:08 +0500

    updated

--- shop/urls.py
+++ shop/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path(
-        'create-list/',
-        CreateListShopAPIView.as_view()
-    ),
-    path(
-        'detail-delete-update/<int:pk>/',
-        DetailDeleteUpdateShopAPIView.as_view()
-    ),
+    path('list/', ListShopAPIView.as_view()),
+    path('create/', CreateShopAPIView.as_view()),
+    path('detail/<int:pk>/', RetrieveShopAPIView.as_view()),
+    path('update/<int:pk>/', UpdateShopAPIView.as_view()),
+    path('delete/<int:pk>/', DestroyShopAPIView.as_view()),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,125 +1,29 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.exceptions import ValidationError
+from rest_framework import generics
+
 from .models import Shop
 from .serializer import ShopSerializer
 
-# Create your views here.
+
+class ListShopAPIView(generics.ListAPIView):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
 
 
-class CreateListShopAPIView(APIView):
-
-    def get(self, request):
-
-        shops = Shop.objects.all()
-
-        serializer = ShopSerializer(
-            shops,
-            many=True
-        )
-
-        return Response({
-            'count': shops.count(),
-            'msg': 'list',
-            'status': status.HTTP_200_OK,
-            'data': serializer.data
-        })
-
-    def post(self, request):
-
-        serializer = ShopSerializer(
-            data=request.data
-        )
-
-        if serializer.is_valid():
-            serializer.save()
-
-            return Response({
-                'msg': 'Created',
-                'status': status.HTTP_201_CREATED,
-                'data': serializer.data
-            })
-
-        raise ValidationError({
-            'error': serializer.errors,
-            'status': status.HTTP_400_BAD_REQUEST
-        })
+class CreateShopAPIView(generics.CreateAPIView):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
 
 
-class DetailDeleteUpdateShopAPIView(APIView):
+class RetrieveShopAPIView(generics.RetrieveAPIView):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
 
-    def get_object(self, pk):
 
-        shop = Shop.objects.filter(
-            pk=pk
-        ).first()
+class UpdateShopAPIView(generics.UpdateAPIView):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
 
-        if not shop:
-            raise ValidationError({
-                'msg': 'Shop not found',
-                'status': status.HTTP_400_BAD_REQUEST
-            })
 
-        return shop
-
-    def get(self, request, pk):
-
-        serializer = ShopSerializer(
-            self.get_object(pk)
-        )
-
-        return Response({
-            'msg': 'detail',
-            'status': status.HTTP_200_OK,
-            'data': serializer.data
-        })
-
-    def put(self, request, pk):
-
-        serializer = ShopSerializer(
-            instance=self.get_object(pk),
-            data=request.data
-        )
-
-        serializer.is_valid(
-            raise_exception=True
-        )
-
-        serializer.save()
-
-        return Response({
-            'msg': 'updated',
-            'status': status.HTTP_200_OK,
-            'data': serializer.data
-        })
-
-    def patch(self, request, pk):
-
-        serializer = ShopSerializer(
-            instance=self.get_object(pk),
-            data=request.data,
-            partial=True
-        )
-
-        serializer.is_valid(
-            raise_exception=True
-        )
-
-        serializer.save()
-
-        return Response({
-            'msg': 'updated',
-            'status': status.HTTP_200_OK,
-            'data': serializer.data
-        })
-
-    def delete(self, request, pk):
-
-        self.get_object(pk).delete()
-
-        return Response({
-            'msg': 'deleted',
-            'status': status.HTTP_204_NO_CONTENT
-        })
+class DestroyShopAPIView(generics.DestroyAPIView):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
